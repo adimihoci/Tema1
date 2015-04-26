2fdf38f26930e944a70410af6fb8b7f9291230c0
adimihoci <adi.mihoci@gmail.com>
2015-04-26 11:53:36 +0300

    Tema1

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Tema
+{
+    partial class Tema1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button = new System.Windows.Forms.Button();
+            this.languages_list = new System.Windows.Forms.ListBox();
+            this.text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Button
+            // 
+            this.Button.Location = new System.Drawing.Point(953, 172);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(120, 66);
+            this.Button.TabIndex = 0;
+            this.Button.Text = "Button";
+            this.Button.UseVisualStyleBackColor = true;
+            this.Button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // languages_list
+            // 
+            this.languages_list.FormattingEnabled = true;
+            this.languages_list.ItemHeight = 16;
+            this.languages_list.Location = new System.Drawing.Point(620, 172);
+            this.languages_list.Name = "languages_list";
+            this.languages_list.Size = new System.Drawing.Size(120, 132);
+            this.languages_list.TabIndex = 1;
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(302, 172);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(139, 22);
+            this.text.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter a number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(620, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Choose a language";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(953, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Press the button";
+            // 
+            // Tema1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1376, 536);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.languages_list);
+            this.Controls.Add(this.Button);
+            this.Name = "Tema1";
+            this.Text = "Tema1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.ListBox languages_list;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Humanizer;
+
+namespace Tema
+{
+    public partial class Tema1 : Form
+    {
+        public Tema1()
+        {
+            InitializeComponent();
+            Load_Items_to_ListBox();
+        }
+
+        private void Load_Items_to_ListBox()
+        {
+            languages_list.Items.Add("English");
+            languages_list.Items.Add("French");
+            languages_list.Items.Add("Italian");
+            languages_list.Items.Add("Portuguese");
+            languages_list.Items.Add("Romanian");
+            languages_list.Items.Add("German");
+            languages_list.Items.Add("Arabic");
+        }
+
+        public static bool IsNumeric(object expression)
+        {
+            double retNum;
+
+            bool isNum = Double.TryParse(Convert.ToString(expression), NumberStyles.Any, NumberFormatInfo.InvariantInfo, out retNum);
+            return isNum;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            string new_text = text.Text;
+            
+            if (!IsNumeric(new_text))
+                MessageBox.Show("Enter a number !!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            if ((string)languages_list.SelectedItem == null)
+                MessageBox.Show("Choose a language !!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            int number = Convert.ToInt32(new_text);
+            string language = (string)languages_list.SelectedItem;
+
+            switch (language)
+            {
+                case "English":
+                    MessageBox.Show(number.ToWords(new CultureInfo("en-US")));
+                    break;
+                case "French":
+                    MessageBox.Show(number.ToWords(new CultureInfo("fr")));
+                    break;
+                case "Italian":
+                    MessageBox.Show(number.ToWords(new CultureInfo("it")));
+                    break;
+                case "Portuguese":
+                    MessageBox.Show(number.ToWords(new CultureInfo("pt")));
+                    break;
+                case "Romanian":
+                    MessageBox.Show(number.ToWords(new CultureInfo("ro")));
+                    break;
+                case "German":
+                    MessageBox.Show(number.ToWords(new CultureInfo("de")));
+                    break;
+                case "Arabic":
+                    MessageBox.Show(number.ToWords(new CultureInfo("ar-sa")));
+                    break;
+            }
+
+        }
+    }
+}
